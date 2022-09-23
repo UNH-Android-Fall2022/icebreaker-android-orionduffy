3672adbe2e7a2c0c1e3d04e04dddbed1f75bb5e0
orionduffy <orionbduffy@gmail.com>
2022-09-22 21:06:43 -0400

    Added viewbinding and debug lines

--- app/src/main/java/com/example/icebreaker/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.icebreaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.icebreaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "Icebreaker"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetQuestion.setOnClickListener {
+            Log.d(TAG, "Button Get Question was pressed")
+        }
+
+        binding.btnSubmit.setOnClickListener {
+            Log.d(TAG, "Button Submit was pressed")
+        }
     }
 }
