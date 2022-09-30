d39b6ae9fb1c5d3c1d3ee21c26a03fdd40b1d645
orionduffy <orionbduffy@gmail.com>
2022-09-29 21:15:48 -0400

    Added database communication (Incomplete)

--- app/src/main/java/com/example/icebreaker/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.icebreaker.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val TAG = "Icebreaker"
+    private val db = Firebase.firestore
+    private var questionBank: MutableList<Question> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,21 +22,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getQuestionsFromFirebase()
+
         binding.btnGetQuestion.setOnClickListener {
             Log.d(TAG, "Button Get Question was pressed")
-            getQuestionsFromFirebase()
         }
 
         binding.btnSubmit.setOnClickListener {
             Log.d(TAG, "Button Submit was pressed")
-
-            val firstName = binding.txtFirstName
-            Log.d(TAG, "First name is ${firstName.text}")
-            firstName.setText("")
         }
     }
 
     private fun getQuestionsFromFirebase () {
         Log.d(TAG, "Fetching questions from database...")
+        db.collection("questions")
+            .get()
+            .addOnSuccessListener { collection ->
+                for (document in collection) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+
+                    val question = document.toObject(Question::class.java)
+                    Log.d(TAG, question.text)
+                    questionBank.add(question)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+    private fun writeUserToFirebase() {
+        val firstName = binding.txtFirstName
+        val lastName = binding.txtLastName
+        val preferredName = binding.txtPreferredName
+        val answer = binding.txtAnswer
+
+        val user = hashMapOf(
+            "firstName" to firstName.text.toString(),
+            "lastName" to lastName.text.toString(),
+            "preferredName" to preferredName.text.toString(),
+            "answer" to answer.text.toString()
+        )
+
+        firstName.setText("")
+        lastName.setText("")
+        preferredName.setText("")
+        answer.setText("")
     }
 }
